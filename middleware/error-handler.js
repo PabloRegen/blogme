b99347786ce9928838f7c595a5d2bbd9bb92c65a
Pablo Regen <pabloregen@users.noreply.github.com>
2017-03-13 00:45:41 -0400

    Add temp logs to error-handler.js to fix isCustomBlogmeError bug

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,13 +6,18 @@ module.exports = function(environment) {
 	return function(err, req, res, next) {
 		console.log(err);
 
+
 		if (res.headersSent) {
 			console.log('res.headersSent === true');
-			// the headers have already been sent to the client so we can't start writing a new response
-			// therefore delegate error to Express's default error handling mechanism which kills the connection
+			/* the headers have already been sent to the client so we can't start writing a new response */
+			/* therefore delegate error to Express' default error handling mechanism which kills the connection */
     		return next(err);
   		} else {
   			let errorCode;
+
+  			// temporary logs to fix isCustomBlogmeError bug
+			console.log(`isCustomBlogmeError: ${err.isCustomBlogmeError}`);
+			console.log(`statusCode: ${err.statusCode}`);
 
   			if (err.isCustomBlogmeError) {
   				errorCode = err.statusCode;
@@ -23,15 +28,12 @@ module.exports = function(environment) {
 	  		res.status(errorCode);
 
 			if (environment === 'development') {
-				// display the error massage along with stack for debugging
 				res.render('error', {title: 'error', errorTitle: err.message, errorStacktrace: err.stack});
 			} else {
 				if (errorCode >= 400 && errorCode < 500) {
-					// client error -> display the error massage
 					res.render('error', {title: 'error', errorTitle: err.message});
 				} else {
-					// non client error -> display short description of a standard HTTP response status code
-					// to prevent exposing internal data
+					/* send short description of standard HTTP response status code to avoid exposing internal data */
 					res.render('error', {title: 'error', errorTitle: http.STATUS_CODES[errorCode]});
 				}
 			}
