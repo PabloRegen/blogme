ca711c33195a1d5305b906d7204d9515e1e34234
Pablo Regen <pabloregen@users.noreply.github.com>
2017-03-14 17:01:06 -0400

    Introduce user soft delete scenario on fetch-current-user.js

--- middleware/fetch-current-user.js
+++ middleware/fetch-current-user.js
@@ -11,16 +11,17 @@ module.exports = function(knex) {
 			return Promise.try(() => {
 				return knex('users').where({id: req.session.userId});
 			}).then((users) => {
-				if (users.length === 0) {
-					/* User still logged in but no longer exists in the db so log user out */
-					// need to check user soft delete and suspended cases as well!
-					req.session.destroy;
-					// next()???
+				let user = users[0];
+
+				if (users.length === 0 || user.deletedAt != null) {
+					/* User still logged in but no longer exists in the db or was soft deleted, so log user out */
+					req.session.destroy();
 				} else {
 					/* make current user available application-wide */
-					req.currentUser = users[0];
-					next();
+					req.currentUser = user;
 				}
+
+				next();
 			});
 		}
 	};
