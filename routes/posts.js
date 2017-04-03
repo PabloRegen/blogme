9100d8677e4c3aa5559ac65f370304e46bcfd9d9
Pablo Regen <pabloregen@users.noreply.github.com>
2017-04-03 14:30:17 -0400

    Add checkit validations to posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,25 +2,45 @@
 
 const Promise = require('bluebird');
 const expressPromiseRouter = require('express-promise-router');
+const checkit = require('checkit');
 const rfr = require('rfr');
 
 const requireSignin = rfr('middleware/require-signin');
+
+let logReqBody = function(environment, reqBody, whichReqBody) {
+	if (environment === 'development') {
+		console.log(whichReqBody);
+		console.log(reqBody);
+	}
+};
+
+let logError = function(environment, err, errorType) {
+	if (environment === 'development') {
+		console.log('error type is: ', errorType);
+		console.log('error is:');
+		console.log(err);
+	}
+};
 
 module.exports = function(knex, environment) {
 	let router = expressPromiseRouter();
 
 	/* create */
 	router.get('/create', requireSignin, (req, res) => {
+		logReqBody(environment, req.body, 'create get! req.body:');
+
 		res.render('posts/create');
 	});
 
 	router.post('/create', requireSignin, (req, res) => {
-		if (environment === 'development') {
-			console.log('create post - req.body:');
-			console.log(req.body)
-		}
+		logReqBody(environment, req.body, 'create post! req.body:');
 
 		return Promise.try(() => {
+			return checkit({
+				title: 'required',
+				body: 'required'
+			}).run(req.body);
+		}).then(() => {
 			return knex('posts').insert({
 				userId: req.currentUser.id,
 				title: req.body.title,
@@ -29,45 +49,52 @@ module.exports = function(knex, environment) {
 			}).returning('id');
 		}).then((postId) => {
 			res.redirect(`/posts/${postId}`);
+		}).catch(checkit.Error, (err) => {
+			logError(environment, err, 'checkitError');
+
+			res.render('posts/create', {errors: err.errors});
 		});
 	});
 
 	/* read */
 	router.get('/:id', (req, res) => {
+		logReqBody(environment, req.body, 'read get! req.body:');
+
 		return Promise.try(() => {
 			return knex('posts').where({id: req.params.id});
 		}).then((posts) => {
 			if (posts.length === 0) {
 				throw new Error('The selected post does not exist');
 			} else {
-				res.render('posts/read', {
-					post: posts[0]
-				});
+				res.render('posts/read', {post: posts[0]});
 			}
 		});
 	});
 
 	/* edit */
 	router.get('/:id/edit', requireSignin, (req, res) => {
+		logReqBody(environment, req.body, 'edit get! req.body:');
+
 		return Promise.try(() => {
 			return knex('posts').where({id: req.params.id});
 		}).then((posts) => {
 			if (posts.length === 0) {
 				throw new Error('The selected post does not exist');
 			} else {	
-				res.render('posts/edit', {
-					post: posts[0]
-				});
+				res.render('posts/edit', {post: posts[0]});
 			}
 		});
 	});
 
 	router.post('/:id/edit', requireSignin, (req, res) => {
-		if (environment === 'development') {
-			console.log(req.body)
-		}
+		logReqBody(environment, req.body, 'edit post! req.body:');
 
 		return Promise.try(() => {
+			return checkit({
+				title: 'required',
+				body: 'required'
+			}).run(req.body);
+		}).then(() => {
 			return knex('posts')
 				.where({id: req.params.id})
 				.update({
@@ -79,6 +106,10 @@ module.exports = function(knex, environment) {
 			;
 		}).then(() => {
 			res.redirect(`/posts/${req.params.id}`);
+		}).catch(checkit.Error, (err) => {
+			logError(environment, err, 'checkitError');
+
+			res.render('posts/edit', {errors: err.errors});
 		});
 	});
 
