4dad6feb159308fe70664a20b20a338b00bff45b
Pablo Regen <pabloregen@users.noreply.github.com>
2017-06-16 11:28:57 -0400

    Update routes within routes/uploads.js

--- routes/uploads.js
+++ routes/uploads.js
@@ -26,6 +26,7 @@ module.exports = function(knex, environment) {
 	});
 
 	let storeUpload = Promise.promisify(multer({storage: storage}).single('image'));
+	// let storeUpload = Promise.promisify(multer({storage: storage}).array('images'));
 	// req.files is array of `postimages` files
 
 	/* upload */
@@ -42,31 +43,46 @@ module.exports = function(knex, environment) {
 			logReqBody(environment, 'POST/upload req.body: ', req.body);
 			logReqFile(environment, 'POST/upload req.file: ', req.file);
 
-			// return Promise.try(() => {
-				return knex('postimages').insert({
-					userId: req.currentUser.id,
-					path: req.file.path,
-					currentName: req.file.filename,
-					originalName: req.file.originalname,
-					size: Math.round(req.file.size / 1000),
-					caption: req.body.caption,
-					ownerName: req.body.owner,
-					licenseType: req.body.license,
-					originalURL: req.body.url,
-					// height: ,
-					// width: ,
-					// dated: 
+			if (req.file == null) {
+				res.render('uploads/upload', {
+					body: req.body,
+					message: 'You must choose an image'
 				});
-			// });
-		}).then(() => {
-			res.end();
+			} else {
+				return Promise.try(() => {
+					return knex('postimages').insert({
+						userId: req.currentUser.id,
+						path: req.file.path,
+						currentName: req.file.filename,
+						originalName: req.file.originalname,
+						size: Math.round(req.file.size / 1000),
+						caption: req.body.caption,
+						ownerName: req.body.owner,
+						licenseType: req.body.license,
+						originalURL: req.body.url,
+						// height: ,
+						// width: ,
+						// dated: 
+					}).returning('id');
+				}).then((imageIds) => {
+					res.redirect(`/uploads/${imageIds[0]}`);
+				});
+			}
 		});
 		// pic: req.files != null && req.files.postPic != null ? req.files.postPic[0] : undefined,
 	});
 
 	/* edit */
 	router.get('/:id/edit', requireSignin(environment), (req, res) => {
-
+		return Promise.try(() => {
+			return knex('postimages').where({id: req.params.id});
+		}).then((images) => {
+			if (images.length === 0) {
+				throw new Error('The selected image does not exist');
+			} else {
+				res.render('uploads/edit', {image: images[0]});
+			}
+		});
 	});
 
 	router.post('/:id/edit', requireSignin(environment), (req, res) => {
@@ -74,13 +90,31 @@ module.exports = function(knex, environment) {
 	});
 
 	/* delete */
-	router.get('/:id/delete', (req, res) => {
+	router.post('/:id/delete', requireSignin(environment), (req, res) => {
+		logReqBody(environment, 'POST/:id/delete req.body:', req.body);
 
+		return Promise.try(() => {
+			return knex('postimages').where({id: req.params.id}).update({deletedAt: knex.fn.now()});
+		}).then(() => {
+			res.redirect('/uploads/upload');
+		});
 	});
 
-	/* display */
+	/* display 1 image */
 	router.get('/:id', (req, res) => {
+		logReqBody(environment, 'GET/:id req.body: ', req.body);
 
+		return Promise.try(() => {
+			return knex('postimages').where({id: req.params.id});
+		}).then((images) => {
+			console.log(images);
+
+			if (images.length === 0) {
+				throw new Error('The selected image does not exist');
+			} else {
+				res.render('uploads/display', {image: images[0]});
+			}
+		});
 	});
 
 	return router;
